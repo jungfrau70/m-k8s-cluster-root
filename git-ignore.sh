507c9f233b18050ec56c9ad16ca7c4637b46cf09
InHwan Jung <inhwan.jung@gmail.com>
2022-05-25 19:37:14 -0700

    2022_05_25_07_37_PM

--- git-ignore.sh
+++ git-ignore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #git rm -r --cached .
-git rm --cached venv/* -r
-git rm --cached postgres/postgres-data/* -r
+#git rm --cached venv/* -r
+#git rm --cached postgres/postgres-data/* -r
